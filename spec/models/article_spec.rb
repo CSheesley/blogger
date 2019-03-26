543d26dcbfece3e09a3ebed01329fda70458776e
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-26 07:21:24 -0600

    Adds route, controller, controller action, and view for articles

--- spec/models/article_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Article, type: :model do
-  describe 'Validations' do
-    it {should validate_presence_of(:title)}
-    it {should validate_presence_of(:body)}
-  end
-end
