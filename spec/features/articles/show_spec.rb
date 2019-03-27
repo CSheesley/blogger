e6d2cd2410bdff2fd5a2cdffb4ae4fb2abd3a946
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-27 08:34:50 -0600

    Adds delete functionality for an article

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :feature do
   describe "as a user" do
-    describe "when I visit and article show page" do
+    context "when I visit and article show page" do
       it 'displays the information of that article' do
 
         article = Article.create(title: "Test Article", body: "Hopefulyy this works!")
@@ -16,14 +16,15 @@ RSpec.describe Article, type: :feature do
         expect(page).to have_content(article.body)
       end
 
-      it 'has a button, that when clicked deletes the book' do
+    context 'when I click on the delete button'  
+      it 'should return me to the home page where this books info no longer exists' do
 
         article_1 = Article.create(title: "Test Article", body: "Hopefulyy this works!")
         article_2 = Article.create(title: "Title 2", body: "Body 2")
 
         visit article_path(article_1)
 
-        click_on  "Delete"
+        click_on "Delete"
 
         expect(current_path).to eq(articles_path)
         expect(page).not_to have_content(article_1.title)
